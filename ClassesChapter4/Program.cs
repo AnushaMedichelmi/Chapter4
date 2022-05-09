489af0504fedf603cc2590420ca30e618f42ee6f
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-09 10:21:27 +0530

    Example of classes and objects

--- ClassesChapter4/Program.cs
+++ ClassesChapter4/Program.cs
@@ -9,37 +9,62 @@ namespace ClassesChapter4
     internal class Program
     {
         static void Main(string[] args)
-         {
+        {
 
             //Example-1
 
-        /* Message rainMessage = new Message("Hello, world\n" +"Chinese Democracy is done and it's November\n" +"Is it raining?");
-         rainMessage.Print();
-         Console.WriteLine();*/
-        // }
-         
+            /* Message rainMessage = new Message("Hello, world\n" +"Chinese Democracy is done and it's November\n" +"Is it raining?");
+             rainMessage.Print();
+             Console.WriteLine();*/
+            // }
+
 
             //Example-2
 
-        /*int radius;
-        float area;
-        
-        public Program(int radius)
-        {
-            this.radius = radius;
-            area = (float)(Math.PI * Math.Pow(radius, 2));
-        }
-        
-        public int Radius
-        {
-            get { return radius; }
-        }
+            /*int radius;
+            float area;
 
-        public float Area
-        {
-            get { return area; }
+            public Program(int radius)
+            {
+                this.radius = radius;
+                area = (float)(Math.PI * Math.Pow(radius, 2));
+            }
+
+            public int Radius
+            {
+                get { return radius; }
+            }
+
+            public float Area
+            {
+                get { return area; }
+            }
+        }*/
+
+            //Example-3
+
+            // create circle objects
+            /* Circle circle0 = new Circle(0);
+             Circle circle1 = new Circle(1);
+             Circle circle2 = new Circle(2);
+             Circle circle3 = new Circle(3);
+             Circle circle4 = new Circle(4);
+             Circle circle5 = new Circle(5);
+             // print object info
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle0.Radius, circle0.Area);
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle1.Radius, circle1.Area);
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle2.Radius, circle2.Area);
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle3.Radius, circle3.Area);
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle4.Radius, circle4.Area);
+             Console.WriteLine("Radius: {0}, Area: {1,5:N2}",
+             circle5.Radius, circle5.Area);
+             Console.WriteLine();
+         }*/
         }
-    }*/
-        }
-}
+    }
 }
