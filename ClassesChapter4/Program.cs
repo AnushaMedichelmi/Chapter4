ac0d46b9d98c2ce90baff3858e8de9a0b415b0cc
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-09 10:12:54 +0530

    Examples on classes and objects

---
+++ ClassesChapter4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesChapter4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+         {
+
+            //Example-1
+
+        /* Message rainMessage = new Message("Hello, world\n" +"Chinese Democracy is done and it's November\n" +"Is it raining?");
+         rainMessage.Print();
+         Console.WriteLine();*/
+        // }
+         
+
+            //Example-2
+
+        /*int radius;
+        float area;
+        
+        public Program(int radius)
+        {
+            this.radius = radius;
+            area = (float)(Math.PI * Math.Pow(radius, 2));
+        }
+        
+        public int Radius
+        {
+            get { return radius; }
+        }
+
+        public float Area
+        {
+            get { return area; }
+        }
+    }*/
+        }
+}
+}
